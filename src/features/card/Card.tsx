dc882061359840c50832e5e0fd92cb2897ef9d8c
uche <thesimplearchitect@gmail.com>
2021-02-24 23:12:01 +0100

    Refactored app: update react and added typescript

---
+++ src/features/card/Card.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './card.css';
+import { Word } from '../../App';
+
+interface CardProps {
+  word: Word;
+}
+
+type Lang = 'en' | 'de';
+
+const Card: React.FC<CardProps> = ({ word }) => {
+  const [lang, setLang] = React.useState<Lang>('de');
+  const [show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    setShow(!!word);
+  }, [word])
+
+  const getLangText = () => {
+    return lang === 'en' ? 'DE' : 'EN';
+  }
+
+  const getWord = () => {
+    return lang === 'en' ? word.english : word.german;
+  }
+
+  const switchLang = () => {
+    setLang(lang === 'en' ? 'de' : 'en');
+  }
+
+  return (
+    <>
+      {show &&
+        <div className="card">
+          <div className="card-heading">
+            <div className="card-lang" onClick={switchLang}>{getLangText()}</div>
+          </div>
+          <div className="card-content">
+              {getWord()}
+            </div>
+        </div>
+      }
+    </>
+    
+  );
+}
+
+export default Card;

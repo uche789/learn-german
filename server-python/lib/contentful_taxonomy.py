9599305e33d8bdf73127afde6bfb8df8fd7f9a4c
uche789 <3889297+uche789@users.noreply.github.com>
2024-10-26 14:38:15 +0200

    extend taxonomy.py

--- server-python/lib/contentful_taxonomy.py
+++ server-python/lib/contentful_taxonomy.py
@@ -5,6 +5,7 @@ import json
 def get_taxonomies():
     filePath = './output/taxonomy.json'
     result = {}
+    resultAlt = {}
     with open(filePath, 'r') as file:
         data = json.load(file)
         mapList = {}
@@ -18,21 +19,27 @@ def get_taxonomies():
             if list(item['broader']).__len__() > 0:
                 if mapList.get(item['broader'][0]['sys']['id']):
                     category = mapList.get(item['broader'][0]['sys']['id'])
-                    print(result.get(category['label']))
                     if result.get(category['label']):
                         # do something
                         temp = result.get(category['label'])
                         temp.__setitem__(item['prefLabel']['en-US'], item['sys']['id'])
-                        print(result.keys())
+                        tempAlt = resultAlt.get(category['label'])
+                        tempAlt.__setitem__(item['sys']['id'], item['prefLabel']['en-US'])
                     else:
                         result.__setitem__(category['label'], {
                             item['prefLabel']['en-US']: item['sys']['id'],
                         })
-    print(result)
-    
+                        resultAlt.__setitem__(category['label'], {
+                            item['sys']['id']: item['prefLabel']['en-US'],
+                        })
+
     categoryFilePath = './output/category.json'
     dataFile = open(categoryFilePath, 'w')
     dataFile.write(json.dumps(result))
+
+    categoryFilePathAlt = './output/categoryAlt.json'
+    dataFile = open(categoryFilePathAlt, 'w')
+    dataFile.write(json.dumps(resultAlt))
 
         
                 

12a681ae051123921d0b4e498f1b35f65e58fa72
uche789 <3889297+uche789@users.noreply.github.com>
2024-11-08 10:37:14 +0100

    remove unnecessary files

--- build/assets/arrowLeft-BpeEJoBC.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-BhI1tFmG.js";const o=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",...t},e.createElement("path",{d:"M10 19L3 12M3 12L10 5M3 12H21",stroke:"#3F3F46",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"}));export{o as default};
